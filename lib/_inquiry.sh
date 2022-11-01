a82fbb640be9e90a0cb13c2848fdfd6aa9b244fb
mrx069 <79999858+mrx069@users.noreply.github.com>
2022-11-01 19:09:54 +0100

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -3,7 +3,7 @@
 get_mysql_root_password() {
   
   print_banner
-  printf "${WHITE} 💻 Insira senha padrão para o sistema:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Geben Sie das Standardkennwort für das System ein:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " mysql_root_password
 }
@@ -11,7 +11,7 @@ get_mysql_root_password() {
 get_instancia_add() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o nome da empresa a ser configurada (Utilizar Letras minusculas):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Geben Sie den Namen der zu konfigurierenden Firma ein (Verwenden Sie Kleinbuchstaben):${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " instancia_add
 }
@@ -19,7 +19,7 @@ get_instancia_add() {
 get_max_whats() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o numero maximo de conexões que a empresa ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Geben Sie die maximale Anzahl von Verbindungen ein, die das Unternehmen ${instancia_add} registrieren kann:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " max_whats
 }
@@ -27,7 +27,7 @@ get_max_whats() {
 get_max_user() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o numero maximo de atendentes que a empresa ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Geben Sie die maximale Anzahl an Teilnehmern ein, die das Unternehmen ${instancia_add} registrieren kann:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " max_user
 }
@@ -35,7 +35,7 @@ get_max_user() {
 get_frontend_url() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o domínio do FRONTEND/PAINEL para a ${instancia_add}:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Geben Sie die FRONTEND/PANEL-Domain für ${instancia_add} ein:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " frontend_url
 }
@@ -43,7 +43,7 @@ get_frontend_url() {
 get_backend_url() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o domínio do BACKEND/API para a ${instancia_add}:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Geben Sie die BACKEND/PANEL-Domain für ${instancia_add} ein:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " backend_url
 }
@@ -51,7 +51,7 @@ get_backend_url() {
 get_frontend_port() {
   
   print_banner
-  printf "${WHITE} 💻 Digite a porta do FRONTEND para a ${instancia_add}; Ex: 3000 A 3999 ${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Geben Sie den FRONTEND-Port für ${instancía_add} ein; Ex: 3000 A 3999 ${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " frontend_port
 }
@@ -60,7 +60,7 @@ get_frontend_port() {
 get_backend_port() {
   
   print_banner
-  printf "${WHITE} 💻 Digite a porta do BACKEND para esta instancia; Ex: 4000 A 4999 ${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Geben Sie den BACKEND-Port für diese Instanz ein; Ex: 4000 A 4999 ${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " backend_port
 }
@@ -68,7 +68,7 @@ get_backend_port() {
 get_redis_port() {
   
   print_banner
-  printf "${WHITE} 💻 Digite a porta do REDIS / AGENDAMENTO MSG para a ${instancia_add}; Ex: 5000 A 5999 ${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Geben Sie den REDIS-Port / MSG SCHEDULE für ${instancia_add} ein; Ex: 5000 A 5999 ${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " redis_port
 }
@@ -95,10 +95,10 @@ software_update() {
 inquiry_options() {
   
   print_banner
-  printf "${WHITE} 💻 Bem vindo(a) ao instalador Owen Zap, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Willkommen beim Whaticket-Installationsprogramm. Wählen Sie unten Ihre nächste Aktion aus!${GRAY_LIGHT}"
   printf "\n\n"
-  printf "   [1] Instalar o OwenZap\n"
-  printf "   [2] Atualizar o OwenZap\n"
+  printf "   [1] Installieren Sie Whaticket\n"
+  printf "   [2] Aktualisieren Sie Whaticket\n"
   printf "\n"
   read -p "> " option
 
